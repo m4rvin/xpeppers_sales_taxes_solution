b39f4cb073d0bf6439041e027cd9989e2e74d443
Daniele Angelucci <angelucci.dnl@gmail.com>
2017-05-17 16:04:20 +0200

    Added mockito to pom and added a first input parsing test.

---
+++ src/test/java/PurchasedItemTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PurchasedItemTest
+{
+
+	@Test
+	public void testPurchasedItemParsing()
+	{
+		fail("Not yet implemented");
+	}
+
+}
