5a24f38a5421711c3970536f4fd64fbbb8f6ffe9
Daniele Angelucci <angelucci.dnl@gmail.com>
2017-05-17 15:42:59 +0200

    Added commandline parsing to main.

--- src/main/java/me/danieleangelucci/main/Main.java
+++ src/main/java/me/danieleangelucci/main/Main.java
@@ -5,7 +5,30 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.out.println("HELLO!");
+		
+		checkCommandlineArgs(args);
+		
+		String categoriesFilePath = args[0];
+		String inputFilePath = args[1];
+
+		System.out.println("Welcome to \"Sales taxes problem\"!");
+		System.out.println("Loading categories file: " + categoriesFilePath);
+		System.out.println("Using input file: " + inputFilePath);
+
+		
+	}
+	
+	private static void checkCommandlineArgs(String[] args) {
+		
+		if(args.length == 2)
+			return;
+
+		if(args.length < 2)
+	        System.err.println("Too few arguments.");
+		if(args.length > 2)
+	        System.err.println("Too many arguments.");
+        System.err.println("Commandline should be:\njava -jar \"path_to_jar\" \"categories_filepath\" \"input_filepath\"");
+		System.exit(1);
 	}
 
 }
